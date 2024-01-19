85c2e2d6a5cf9f461333b7eb30263100f59d2bc1
pe4enka22 <missis.bug@gmail.com>
2024-01-19 17:51:54 +0200

    14 added

--- 14.js
+++ 14.js
@@ -3,34 +3,40 @@
 //     Наприклад: 1000 метрів це 1 кілометр;  32 метри це 0,032 кілометра і т.д.
 //     Підказка(https://ukr-lifehacks.ed-era.com/rozdil-9/zvyazok_chislivnykiv)
 
-let meter = 1000;
-let km = meter / 1000;
-//integer till 1000
-if(meter<0){
-    console.log('Величина не може бути негативною');
-}else if(meter===0){
-    console.log(`${meter} метрів це ${km} кілометрів`);
-}else if(meter%10===1){
-    console.log(`${meter} метр це ${km} кілометрa`);
-}else if(meter%10===2 || meter%10===3 || meter%10===4){
-    console.log(`${meter} метри це ${km} кілометрa`);
-}else if( meter<1000 && meter%10===5 ||
-    meter%10===6 ||
-    meter%10===7 ||
-    meter%10===8 ||
-    meter%10===9 ||
-    meter%10===0) {
-    console.log(`${meter} метрів це ${km} кілометра`);
-}else if(meter===1000 && meter>=1000){
-    console.log(`${meter} метрів це ${km} кілометр`);
-}else if(meter===2000 || meter===3000 || meter===4000){
-    console.log(`${meter} метрів це ${km} кілометри`);
-}else if(meter>=5000 && meter%1000===0){
-    console.log(`${meter} метрів це ${km} кілометрів`);
-}else if(!Number.isInteger(meter)){
-    console.log(`${meter} метра це ${km} кілометрa`);
-} else {
-    console.log(`${meter} метра це ${km} кілометрa`);
+
+function toKilometers(meters){
+    let kilometers = meters/1000;
+    let firstPart = meters + " метр"
+    let secondPart = " це " + kilometers + " кілометр"
+
+    if(meters<0){
+        return "Не може бути негативне"
+    }
+    else if(meters===0){
+        return firstPart + "ів" + secondPart + "iв"
+    } else if(meters%10===1){
+        return firstPart + secondPart + "а"
+    } else if(meters%10===2 || meters%10===3 || meters%10===4){
+        return firstPart + "и" + secondPart + "а"
+    } else if(meters%10===5 ||
+    meters%10===6 ||
+    meters%10===7 ||
+    meters%10===8 ||
+    meters%10===9) {
+        return firstPart + "ів" + secondPart + "а"
+    } else if (meters===1000){
+        return  firstPart + "ів" + secondPart
+    } else if (meters===2000 || meters===3000 || meters===4000){
+        return  firstPart + "ів" + secondPart + "и"
+    } else if (meters>=5000 && meters%1000===0){
+    return  firstPart + "ів" + secondPart + "ів"
+} else if (!Number.isInteger(meters)){
+        return firstPart + "а" + secondPart + "а"
+    } else{
+        return інше
+    }
+
 }
-// //if(meter%11===0 || meter%11===1 || meter%11===2 || meter%11===3 && Number.isInteger(meter)){console.log(`${meter} метрів це ${km} кілометрa`)}else // закінчуються нв 11, 12, 13, 14
+
+console.log(toKilometers(149000));
 
